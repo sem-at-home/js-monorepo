6304c620a1b4b6fec0f4b013673f9c83e91a2825
Sem Erik Joop van Velden <vanveldensem@gmail.com>
2024-10-10 13:08:05 +0200

    Configre webpack-merge to merge dev and prod configs

--- webpack-practice/src/index.js
+++ webpack-practice/src/index.js
@@ -4,6 +4,12 @@ import greeting from './greeting';
 
 console.log(greeting);
 
+if(process.env.NODE_ENV !== 'production') {
+    console.log('Development mode');
+} else {
+    console.log('Production mode');
+}
+
 const image = document.createElement('img');
 image.src = odinImage;
 document.body.appendChild(image);
